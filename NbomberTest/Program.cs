5b166684e45561d8adda0ca72a26cdfd23816556
Zahra Bayat <58395585+ZahraBayatgh@users.noreply.github.com>
2023-01-21 07:59:44 +0330

    Add project files.

---
+++ NbomberTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using NBomber.Contracts;
+using NBomber.CSharp;
+using NBomber.Plugins.Http.CSharp;
+
+var httpFactory = HttpClientFactory.Create();
+
+var step = Step.Create("step", httpFactory, async context =>
+{
+    var response = await context.Client.GetAsync("http://localhost:5000/WeatherForecast", context.CancellationToken);
+
+    return response.IsSuccessStatusCode
+        ? Response.Ok(statusCode: (int)response.StatusCode)
+        : Response.Fail(statusCode: (int)response.StatusCode);
+});
+
+var scenario = ScenarioBuilder
+    .CreateScenario("httpScenario", step)
+    .WithWarmUpDuration(TimeSpan.FromSeconds(3))
+    .WithLoadSimulations(Simulation.InjectPerSec
+    (rate: 100, during: TimeSpan.FromSeconds(10)));
+
+NBomberRunner
+    .RegisterScenarios(scenario)
+    .Run();
